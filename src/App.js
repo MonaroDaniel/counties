99d89c56bde138b85db75cf6324a00c8006d5ad8
MonaroDaniel <wolfmonaro@gmail.com>
2023-06-27 11:11:22 -0300

    Slips Randle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
+import axios from 'axios';
+import './Loading.css'
+import Loading from './components/Loading';
 
 function App() {
+  const [slips, setSlips] = useState({});
+  const [removeLoading, setRemoveLoading] = useState(true);
+
+  useEffect(() => {
+    if (!slips.advice) {
+      setRemoveLoading(true)
+      setTimeout(() => {
+          axios
+            .get('https://api.adviceslip.com/advice')
+            .then((res) => {
+              setSlips(res.data.slip)
+            })
+            .catch((err) => console.log(err))
+            .finally(() => {
+              setRemoveLoading(false)
+            })
+          }, 1000)
+        }
+  }, [slips]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Click on the button to general a county</h1>
+      <div class="content">
+        <h2>{slips.advice}</h2>
+        {removeLoading && <Loading/>}
+      </div>
+        <br/>
+      <button className="button" onClick={() => setSlips({})}>Click-me</button>
     </div>
+
   );
 }
 
